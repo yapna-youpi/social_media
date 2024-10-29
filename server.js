4726353f88774f01c168f8b62ec9cf172c0d6f22
yapna-youpi <yapnayoupi@gmail.com>
2024-10-29 13:55:39 +0100

    first commit to add code

---
+++ server.js
@@ -0,0 +1,7 @@
+//about server and principals components
+const express = require("express");
+const app = express();
+
+app.listen('5000', ()=> {
+    console.log("server slistening on port 5000")
+})
